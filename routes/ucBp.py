bb3fd1991d8b44d8580af7e7b7bbe9c475f07bc4
Gustavo Alves <gustavovaladao.4491@aluno.saojudas.br>
2022-09-14 22:12:58 -0300

    adicionando o render_template e o html

--- routes/ucBp.py
+++ routes/ucBp.py
@@ -1,5 +1,5 @@
-# importando o Blueprint
-from flask import Blueprint
+# importando o Blueprint e adicionando render_template
+from flask import Blueprint, render_template
 from ..extensions import db
 from ..models.uc import Uc
 
@@ -8,6 +8,9 @@ ucBp = Blueprint('ucBP', __name__)
 
 @ucBp.route('/uc')
 def uc_list():
-    #return "Teste"
+#    return "Teste"
     #nova adição
-    db.create_all()
+#    db.create_all()
+# adiciona o acesso a banco e a chamada ao render_template
+    ucs_query = Uc.query.all()
+    return render_template('uc_list.html', ucs=ucs_query)
