426ceb4d68a7233a9f66b2a348a158e92e0325ab
Gustavo Alves <gustavovaladao.4491@aluno.saojudas.br>
2022-09-14 21:31:09 -0300

    passo de criação de banco de dados

--- routes/ucBp.py
+++ routes/ucBp.py
@@ -1,9 +1,13 @@
 # importando o Blueprint
 from flask import Blueprint
+from ..extensions import db
+from ..models.uc import Uc
 
 # Instanciar o blueprint
 ucBp = Blueprint('ucBP', __name__)
 
 @ucBp.route('/uc')
 def uc_list():
-    return "Teste"
+    #return "Teste"
+    #nova adição
+    db.create_all()
