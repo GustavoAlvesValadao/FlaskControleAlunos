4fa2ad51910109c87184f845572cbaa4c55e0a7f
Gustavo Alves <gustavovaladao.4491@aluno.saojudas.br>
2022-09-21 20:09:44 -0300

    adicionando abas do formulário

--- routes/ucBp.py
+++ routes/ucBp.py
@@ -1,7 +1,8 @@
 # importando o Blueprint e adicionando render_template
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from ..extensions import db
 from ..models.uc import Uc
+from datetime import date, datetime
 
 # Instanciar o blueprint
 ucBp = Blueprint('ucBP', __name__)
@@ -14,3 +15,21 @@ def uc_list():
 # adiciona o acesso a banco e a chamada ao render_template
     ucs_query = Uc.query.all()
     return render_template('uc_list.html', ucs=ucs_query)
+
+@ucBp.route('uc/create')
+def create_uc():
+    return render_template('uc_create.html')
+
+@ucBp.route('/uc/add', methods=["POST"])
+def add_uc():
+
+    sNome = request.form["nome"]
+    sTipo = request.form["tipo"]
+    dInicio = datetime.strptime(request.form["inicio"], '%Y-%m-%d')
+    dFim = datetime.strptime(request.form["fim"], '%Y-%m-%d')
+
+    uc = Uc(nome=sNome, tipo=sTipo, inicio=dInicio, fim=dFim)
+    db.session.add(uc)
+    db.session.commit()
+
+    return redirect(url_for("ucBp.uc_list"))
