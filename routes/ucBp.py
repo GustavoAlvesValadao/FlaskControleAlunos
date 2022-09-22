a9fd02710547685f73ef71f192d76617cdde6349
Gustavo Alves <gustavovaladao.4491@aluno.saojudas.br>
2022-09-21 22:10:04 -0300

    finalizando crud

--- routes/ucBp.py
+++ routes/ucBp.py
@@ -11,7 +11,7 @@ ucBp = Blueprint('ucBP', __name__)
 def uc_list():
 #    return "Teste"
     #nova adição
-#    db.create_all()
+    db.create_all()
 # adiciona o acesso a banco e a chamada ao render_template
     ucs_query = Uc.query.all()
     return render_template('uc_list.html', ucs=ucs_query)
@@ -30,6 +30,47 @@ def add_uc():
 
     uc = Uc(nome=sNome, tipo=sTipo, inicio=dInicio, fim=dFim)
     db.session.add(uc)
+    db.session.commit()
+
+    return redirect(url_for("ucBp.uc_list"))
+
+#chamar o formulário de alteração
+@ucBp.route('/uc/update/<uc_id>')
+def update_uc(uc_id=0):
+    uc_query = Uc.query.filter_by(id = uc_id).first()
+    return render_template('uc_update.html', uc=uc_query)
+
+@ucBp.route('/uc/upd', methods=["POST"])
+def upd_uc():
+
+    iUc = request.form["id"]
+    sNome = request.form["nome"]
+    sTipo = request.form["tipo"]
+    dInicio = datetime.strptime(request.form["inicio"], '%Y-%m-%d')
+    dFim = datetime.strptime(request.form["fim"], '%Y-%m-%d')
+
+    uc = Uc.query.filter_by(id = iUc).first()
+    uc.nome = sNome
+    uc.tipo = sTipo
+    uc.inicio = dInicio
+    uc.fim = dFim
+    db.session.add(uc)
+    db.session.commit()
+
+    return redirect(url_for("ucBp.uc_list"))
+
+@ucBp.route('/uc/delete/<uc_id>')
+def delete_uc(uc_id=0):
+    uc_query = Uc.query.filter_by(id = uc_id).first()
+    return render_template('uc_delete.html', uc=uc_query)
+
+#rota para apagar de fato
+@ucBp.route('/uc/dlt', methods=["POST"])
+def dlt_uc():
+
+    iUc = request.form["id"]
+    uc = Uc.query.filter_by(id = iUc).first()
+    db.session.delete(uc)
     db.session.commit()
 
     return redirect(url_for("ucBp.uc_list"))
