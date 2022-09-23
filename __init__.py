2c8fad752860ace33460e009f57a9d117f6e8edb
Gustavo Alves <gustavovaladao.4491@aluno.saojudas.br>
2022-09-23 00:45:50 -0300

    ajustando

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from .extensions import db, migrate
 from .routes.ucBp import ucBp
+
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
